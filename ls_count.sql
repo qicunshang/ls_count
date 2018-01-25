a3834add54078af456361d626215199896760692
qicunshang <936794508@qq.com>
2018-01-25 16:51:47 +0800

    修改说明

---
+++ ls_count.sql
@@ -0,0 +1,32 @@
+﻿# Host: 118.89.230.252  (Version: 5.5.56-log)
+# Date: 2018-01-25 16:45:56
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "ls_per_inv"
+#
+
+DROP TABLE IF EXISTS `ls_per_inv`;
+CREATE TABLE `ls_per_inv` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `source_page` varchar(255) NOT NULL DEFAULT '' COMMENT '来源页面',
+  `inv_count` int(11) NOT NULL DEFAULT '0' COMMENT '访问次数',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='单页面访问';
+
+#
+# Structure for table "ls_user"
+#
+
+DROP TABLE IF EXISTS `ls_user`;
+CREATE TABLE `ls_user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `appid` varchar(50) NOT NULL DEFAULT '0' COMMENT 'appid',
+  `secret_key` varchar(255) NOT NULL DEFAULT '0' COMMENT '秘钥',
+  `created_at` bigint(12) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `updated_at` bigint(12) NOT NULL DEFAULT '0' COMMENT '修改时间',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -1,7 +1,5 @@
 @extends("home.countCommon")
 @section("count")
-    <div id="ls_count"></div>
-    <script>
-        ls_count('ls','ls');
-    </script>
+    <script>ls_count('appid','secret');</script>
+    总访问量:<span id="ls_count"></span>, 页面访问量<span id="page_count"></span>
 @endsection
